e5ffddfc5c2c4876a4a6d0ad4ac86957f57c1766
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2022-02-14 15:05:42 +0530

    [tc-101]-test the node CI

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import path from 'path'
 import matter from 'gray-matter'
 import {marked} from 'marked'
 
-//1.Read the md files
-// 2.Process the headers
-// 3.read the content
-// 4.return as object
-// 5.inject into template (html)
-//6.save file to serve them
 
 const readFile = (filename) => {
     const rawFile = fs.readFileSync(filename, 'utf-8')
